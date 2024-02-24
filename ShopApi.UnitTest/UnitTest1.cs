733e33094e32b353978e8b049ec660b00fcf0fcb
Son Dev <sonnq@gmail.com>
2024-02-24 15:22:13 +0700

    the first commit

---
+++ ShopApi.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ShopApi.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ShopApi.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopApi.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
